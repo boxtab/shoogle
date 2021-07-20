d304a7ae12df393ed1808ebfd419329deb580459
Alexander Chernykh <alexander.c@giantleaplab.com>
2021-07-20 14:29:26 +0300

    No new users registered. Found a bug. Model property must match guard

--- app/User.php
+++ app/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    protected $guard_name = 'api';
+    protected $guard_name = 'web';
 
     public function company(): BelongsTo
     {
