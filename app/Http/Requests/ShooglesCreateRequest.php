7b3680b44923c14e4f198ec3a8301680b5738091
Alexander Chernykh <alexander.c@giantleaplab.com>
2021-10-01 15:31:43 +0300

    To create a shoogle, settings validation is enabled only if the user has specified to send notifications.

--- app/Http/Requests/ShooglesCreateRequest.php
+++ app/Http/Requests/ShooglesCreateRequest.php
@@ -28,9 +28,9 @@ class ShooglesCreateRequest extends ApiRequest
             'wellbeingCategoryId'   => ['required', 'integer', 'exists:wellbeing_categories,id'],
             'active'                => ['required', 'boolean'],
             'title'                 => ['nullable', 'min:2', 'max:45'],
-            'reminder'              => ['nullable', 'min:2', 'date_format:"Y-m-d H:i:s"'],
-            'reminderInterval'      => ['nullable', 'string', 'max:1024'],
-            'isReminder'            => ['nullable', 'boolean'],
+            'reminder'              => 'required_if:isReminder,==,true|nullable|min:2|date_format:"Y-m-d H:i:s"',
+            'reminderInterval'      => 'required_if:isReminder,==,true|nullable|string|max:1024',
+            'isReminder'            => ['required', 'boolean'],
             'coverImage'            => ['required', 'min:2', 'max:256'],
         ];
     }
