136ab15ea4145ecede3189041168e8a1ce28e1b3
Alexander Chernykh <alexander.c@giantleaplab.com>
2021-10-15 12:13:56 +0300

    Made the integration of styles into the body of the letter

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -10,7 +10,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/invite/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="./css/invite/style.css">
+{{--    <link rel="stylesheet" href="{{asset('css/invite/style.css')}}">--}}
 </head>
 
 <body>
