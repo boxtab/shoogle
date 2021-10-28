3e9c0f912550a135e698860f8f21edea37898b43
Alexander Chernykh <alexander.c@giantleaplab.com>
2021-10-28 15:39:40 +0300

    Removed from the list of companies. Destroyed companies.

--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -80,6 +80,7 @@ class CompanyRepository extends Repositories
                     ) as contact_person_email,
                     (select count(uc.id) from users as uc where uc.company_id = c.id) as users_count
                 from companies as c
+                where c.deleted_at is not null
                 order by c.name ' . $order . '
             '));
     }
