a69e9a4e9dbff0a963ac6aaa1001732f9e602843
Alexander Chernykh <alexander.c@giantleaplab.com>
2021-12-02 13:14:48 +0300

    Renamed interval name for well-being points statistics

--- app/Traits/WellbeingWeekLabelTrait.php
+++ app/Traits/WellbeingWeekLabelTrait.php
@@ -52,14 +52,14 @@ trait WellbeingWeekLabelTrait
             $todayText = date('Y-m-d');
             $todayTimestamp = strtotime($todayText);
 
-            $weekBeginFormat = date('M d', $mondayTimestamp);
+            $weekStartFormat = date('M d', $mondayTimestamp);
             if ( $todayTimestamp < $sundayTimestamp ) {
                 $weekEndFormat = date('M d', $todayTimestamp);
             } else {
                 $weekEndFormat = date('M d', $sundayTimestamp);
             }
 
-            $dates[] = ['weekBegin' => $weekBeginFormat, 'weekEnd' => $weekEndFormat];
+            $dates[] = ['weekStart' => $weekStartFormat, 'weekEnd' => $weekEndFormat];
             $dateFrom->modify('+1 week');
         }
 
