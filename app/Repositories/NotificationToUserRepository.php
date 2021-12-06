d2136f45a604ca07615ff222d117eb522c301f52
Alexander Chernykh <alexander.c@giantleaplab.com>
2021-12-06 14:29:36 +0300

    Do not display blocked shuggles in the notification list.

--- app/Repositories/NotificationToUserRepository.php
+++ app/Repositories/NotificationToUserRepository.php
@@ -47,7 +47,10 @@ class NotificationToUserRepository extends Repositories
             ->join('notifications_type', 'notifications_type.id', '=', 'notifications_to_user.type_id')
             ->leftJoin('shoogles', 'notifications_to_user.shoogle_id', '=', 'shoogles.id')
             ->whereNull('shoogles.deleted_at')
-//            ->where('shoogles.active', '=', 1)
+            ->where(function ($query) {
+                $query->whereNull('notifications_to_user.shoogle_id')
+                    ->orWhere('shoogles.active', '=', 1);
+            })
             ->where('users.company_id', '=', $companyId)
             ->get([
                 'notifications_to_user.id as id',
